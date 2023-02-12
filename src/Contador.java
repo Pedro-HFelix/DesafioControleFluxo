ce7b2bc8c29f42faa77205c8d1a1b7bafffba704
Pedro-HFelix <pf711363@gmail.com>
2023-02-12 14:33:05 -0300

    Update

--- src/Contador.java
+++ src/Contador.java
@@ -22,9 +22,8 @@ public class Contador {
 
         static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
             //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-            if (parametroUm>parametroDois) {
+            if (parametroUm>parametroDois) 
                 throw new ParametrosInvalidosException();
-            }
             int contagem = parametroDois - parametroUm;
             //realizar o for para imprimir os números com base na variável contagem
             for(int cont=1; cont<=contagem;cont++){
